d7cbc2ae1b2f742f40aa9d7b86e7bb3650539a7c
Esther <144334434+estherishimwe@users.noreply.github.com>
2025-02-09 20:00:13 +0200

    Add files via upload

---
+++ add_ten.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int number, result;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    result = number + 10;
+    printf("The result after adding 10 is: %d\n", result);
+
+    return 0;
+}
+
